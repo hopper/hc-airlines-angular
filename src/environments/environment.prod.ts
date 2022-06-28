9b18607176fa12f0553ccef4780bdea31dd47014
Benoit Burger <benoit@smooss.io>
2022-06-28 15:15:24 +0200

    test docs with real code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  defaultTheme: "theme-light-hopper"
+  defaultTheme: "theme-light-blue"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  defaultTheme: "theme-light-blue"
+  defaultTheme: "theme-light-hopper"
 };
 
 /*
