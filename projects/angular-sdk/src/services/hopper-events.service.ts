93eac4da3332f53f0e1f6b763fcd47770098a86a
ogasparini <ogasparini@hopper.com>
2022-10-06 15:08:34 +0200

    HCAP-269 Add methods in service allowing the creation of exercise events from a component

--- projects/angular-sdk/src/services/hopper-events.service.ts
+++ projects/angular-sdk/src/services/hopper-events.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { Event, CustomerService, CfarOffersBannerDisplay, UiVariant, CfarOffersTakeoverDisplay, CfarForcedChoiceWarning, CfarViewInfo, UiSource, CfarDenyPurchase } from "../apis/hopper-cloud-airline/v1";
+import { Event, CustomerService, CfarOffersBannerDisplay, UiVariant, CfarOffersTakeoverDisplay, CfarForcedChoiceWarning, CfarViewInfo, UiSource, CfarDenyPurchase, CfarExercisePortalDisplay, CfarExerciseVerificationSent, CfarExerciseVerificationComplete, ExerciseStepResult, CfarExerciseCustomerDataComplete, CfarExercisePortalComplete, CfarExerciseCallbackLaunched, CfarExerciseEmailSent, CfarExerciseEmailOpened, CfarExerciseEmailDelivered } from "../apis/hopper-cloud-airline/v1";
 import { HopperProxyService } from "./hopper-proxy.service";
 
 @Injectable()
@@ -15,8 +15,12 @@ export class HopperEventsService extends HopperProxyService {
   }
 
   // ----------------------------------------------------------
-  // CFAR events
+  // CFAR UI events
   // ----------------------------------------------------------
+
+  // *********************
+  // Cfar offers events
+  // *********************
 
   postCreateCfarOffersBannerDisplay(basePath: string, hCSessionId: string, cfarOffersIds: Array<string>, uiVariant: UiVariant): Observable<any> {
     // Init services
@@ -86,6 +90,144 @@ export class HopperEventsService extends HopperProxyService {
       cfarOffersIds: cfarOffersIds,
       uiSource: uiSource,
       type: "cfar_view_info"
+    }
+    
+    return this._customerService.postCustomerEvents(event, hCSessionId);
+  }
+
+  // *********************
+  // Cfar purchase events
+  // *********************
+
+  // *********************
+  // Cfar exercise events
+  // *********************
+  
+  postCreateCfarExercisePortalDisplay(basePath: string, hCSessionId: string, cfarExerciseId: string): Observable<any> {
+    // Init services
+    this._overrideConfiguration(basePath);
+
+    // Build the event
+    let event: CfarExercisePortalDisplay = {
+      occurredDateTime: new Date(),
+      cfarExerciseId: cfarExerciseId,
+      type: "cfar_exercise_portal_display"
+    }
+    
+    return this._customerService.postCustomerEvents(event, hCSessionId);
+  }
+  
+  postCreateCfarExerciseVerificationSent(basePath: string, hCSessionId: string, cfarExerciseId: string): Observable<any> {
+    // Init services
+    this._overrideConfiguration(basePath);
+
+    // Build the event
+    let event: CfarExerciseVerificationSent = {
+      occurredDateTime: new Date(),
+      cfarExerciseId: cfarExerciseId,
+      type: "cfar_exercise_verification_sent"
+    }
+    
+    return this._customerService.postCustomerEvents(event, hCSessionId);
+  }
+  
+  postCreateCfarExerciseVerificationComplete(basePath: string, hCSessionId: string, cfarExerciseId: string, exerciseStepResult: ExerciseStepResult): Observable<any> {
+    // Init services
+    this._overrideConfiguration(basePath);
+
+    // Build the event
+    let event: CfarExerciseVerificationComplete = {
+      occurredDateTime: new Date(),
+      cfarExerciseId: cfarExerciseId,
+      result: exerciseStepResult,
+      type: "cfar_exercise_verification_complete"
+    }
+    
+    return this._customerService.postCustomerEvents(event, hCSessionId);
+  }
+  
+  postCreateCfarExerciseCustomerDataComplete(basePath: string, hCSessionId: string, cfarExerciseId: string, exerciseStepResult: ExerciseStepResult): Observable<any> {
+    // Init services
+    this._overrideConfiguration(basePath);
+
+    // Build the event
+    let event: CfarExerciseCustomerDataComplete = {
+      occurredDateTime: new Date(),
+      cfarExerciseId: cfarExerciseId,
+      result: exerciseStepResult,
+      type: "cfar_exercise_customer_data_complete"
+    }
+    
+    return this._customerService.postCustomerEvents(event, hCSessionId);
+  }
+  
+  postCreateCfarExercisePortalComplete(basePath: string, hCSessionId: string, cfarExerciseId: string, exerciseStepResult: ExerciseStepResult): Observable<any> {
+    // Init services
+    this._overrideConfiguration(basePath);
+
+    // Build the event
+    let event: CfarExercisePortalComplete = {
+      occurredDateTime: new Date(),
+      cfarExerciseId: cfarExerciseId,
+      result: exerciseStepResult,
+      type: "cfar_exercise_portal_complete"
+    }
+    
+    return this._customerService.postCustomerEvents(event, hCSessionId);
+  }
+  
+  postCreateCfarExerciseCallbackLaunched(basePath: string, hCSessionId: string, cfarExerciseId: string, exerciseStepResult: ExerciseStepResult): Observable<any> {
+    // Init services
+    this._overrideConfiguration(basePath);
+
+    // Build the event
+    let event: CfarExerciseCallbackLaunched = {
+      occurredDateTime: new Date(),
+      cfarExerciseId: cfarExerciseId,
+      result: exerciseStepResult,
+      type: "cfar_exercise_callback_launched"
+    }
+    
+    return this._customerService.postCustomerEvents(event, hCSessionId);
+  }
+  
+  postCreateCfarExerciseEmailSent(basePath: string, hCSessionId: string, cfarExerciseId: string): Observable<any> {
+    // Init services
+    this._overrideConfiguration(basePath);
+
+    // Build the event
+    let event: CfarExerciseEmailSent = {
+      occurredDateTime: new Date(),
+      cfarExerciseId: cfarExerciseId,
+      type: "cfar_exercise_email_sent"
+    }
+    
+    return this._customerService.postCustomerEvents(event, hCSessionId);
+  }
+  
+  postCreateCfarExerciseEmailDelivered(basePath: string, hCSessionId: string, cfarExerciseId: string): Observable<any> {
+    // Init services
+    this._overrideConfiguration(basePath);
+
+    // Build the event
+    let event: CfarExerciseEmailDelivered = {
+      occurredDateTime: new Date(),
+      cfarExerciseId: cfarExerciseId,
+      type: "cfar_exercise_email_delivered"
+    }
+    
+    return this._customerService.postCustomerEvents(event, hCSessionId);
+  }
+  
+  postCreateCfarExerciseEmailOpened(basePath: string, hCSessionId: string, cfarExerciseId: string): Observable<any> {
+    // Init services
+    this._overrideConfiguration(basePath);
+
+    // Build the event
+    let event: CfarExerciseEmailOpened = {
+      occurredDateTime: new Date(),
+      cfarExerciseId: cfarExerciseId,
+      type: "cfar_exercise_email_opened"
     }
     
     return this._customerService.postCustomerEvents(event, hCSessionId);
