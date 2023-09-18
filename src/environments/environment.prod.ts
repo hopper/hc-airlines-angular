c725847dba91c7699a1e969bf027058cddb083b7
ogasparini <ogasparini@hopper.com>
2023-09-18 15:03:01 +0200

    HCAP-667 New release nulber

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { Theme } from "src/app/shared/enums/theme.enum";
 export const environment = {
   production: true,
   defaultTheme: Theme.THEME_LIGHT_BLUE,
-  version: 'v6.0.0'
+  version: 'v6.0.1'
 };
