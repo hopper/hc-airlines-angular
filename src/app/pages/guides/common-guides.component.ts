2fcbcc26c28c35705ad6de7d04936e3ff515fcaf
Vivek Mahajan <vmahajan@hopper.com>
2024-01-02 16:46:38 +0200

    pass enviornment variable properly

--- src/app/pages/guides/common-guides.component.ts
+++ src/app/pages/guides/common-guides.component.ts
@@ -21,6 +21,7 @@ export abstract class CommonGuidesComponent implements OnInit, OnDestroy {
   // public basePath: string = 'https://airlines-api.staging.hopper.com/airline/v1.1';
   public imageBasePath: string = ''; // 'http://localhost:4200/';
   public isFakeBackend!: boolean;
+  public env!: string;
 
   protected _unsubcriber: Subject<any>;
   
@@ -116,6 +117,7 @@ export abstract class CommonGuidesComponent implements OnInit, OnDestroy {
     
     // Active fake data in components
     this.isFakeBackend = true;
+    this.env = 'development'
   }
   
   // -----------------------------------------------
